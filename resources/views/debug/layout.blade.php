51481c63a8cf6ba085ad985592ecc54d390bdd97
ruben.matthes <rthem@gmx.de>
2022-11-22 11:27:36 +0100

    add doc

--- resources/views/debug/layout.blade.php
+++ resources/views/debug/layout.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
+{{-- The head of an HTML document is the part
+    that is not displayed in the web browser when the page is loaded. --}}
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
